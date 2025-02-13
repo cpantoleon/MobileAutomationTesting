7aa1e6c5310e845a5ec14a2fca1d26c5b2330553
cpantoleon <67645617+cpantoleon@users.noreply.github.com>
2025-02-13 17:34:02 +0200

    Update TestRunner.java

--- src/test/java/project/test/swag/runners/TestRunner.java
+++ src/test/java/project/test/swag/runners/TestRunner.java
@@ -2,14 +2,16 @@ package project.test.swag.runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.picocontainer.PicoFactory;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        objectFactory = PicoFactory.class,
         features = "src/test/resources/features",
         glue = "project.test.swag.stepDef",
         plugin = { "pretty", "html:target/Test.html", "json:target/Test.json" },
         monochrome = true
 )
-public class TestRunner {
+public class testRunner {
 }
