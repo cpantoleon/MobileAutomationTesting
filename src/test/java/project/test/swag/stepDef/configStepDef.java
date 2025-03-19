c7965e5b90a43f5b12b77b3cf81ad2c75ac2005d
cpantoleon <67645617+cpantoleon@users.noreply.github.com>
2025-03-19 14:04:32 +0200

    Update configStepDef.java

--- src/test/java/project/test/swag/stepDef/configStepDef.java
+++ src/test/java/project/test/swag/stepDef/configStepDef.java
@@ -2,41 +2,32 @@ package project.test.swag.stepDef;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import project.test.swag.actions.AppUI;
 import project.test.swag.actions.LoginActions;
 import project.test.swag.questions.AppQuestions;
 import project.test.swag.questions.LoginQuestions;
 import project.test.swag.utils.AdbConnectedDevices;
+import project.test.swag.utils.DriverCapabilities;
 import project.test.swag.utils.YMLConfig;
 import project.test.swag.utils.YamlConfigReader;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class configStepDef {
 
     LoginActions loginActions;
-
     LoginQuestions loginQuestions;
-
     YamlConfigReader yamlConfigReader;
-
     AppUI appUI;
-
     YMLConfig ymlConfig;
-
     AdbConnectedDevices adbConnectedDevices;
-
     AppQuestions appQuestions;
-
     static AppiumDriver driver;
 
     public configStepDef() {
@@ -68,14 +59,7 @@ public class configStepDef {
 
             YMLConfig.initialize(filePath);
 
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-
-            capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, YMLConfig.configuration.get("platformName"));
-            capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, YMLConfig.configuration.get("deviceName"));
-            capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, YMLConfig.configuration.get("automationName"));
-            capabilities.setCapability("appPackage", YMLConfig.configuration.get("appPackage"));
-            capabilities.setCapability("appActivity", YMLConfig.configuration.get("appActivity"));
-            capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, (Integer) YMLConfig.configuration.get("newCommandTimeout"));
+            DesiredCapabilities capabilities = DriverCapabilities.getCapabilities(YMLConfig.configuration);
 
             driver = new AndroidDriver(new URL(String.valueOf(YMLConfig.configuration.get("url"))), capabilities);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
